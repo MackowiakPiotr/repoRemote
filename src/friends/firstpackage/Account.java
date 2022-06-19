39baa7dfe61ab2ed62f6b77064d763982af5737d
Piotr Mackowiak <mackowiak55@gmail.com>
2022-06-19 22:16:13 +0200

    dodano nowe funkcjonalnosci do klasy account

--- src/friends/firstpackage/Account.java
+++ src/friends/firstpackage/Account.java
@@ -1,8 +1,9 @@
 package friends.firstpackage;
 
 public class Account {
+    private static final int MAX_Debit = -1000;
     private String name;
-    private int balance =0;
+    private int balance = 0;
     private boolean debit = false;
 
 
@@ -17,19 +18,48 @@ public class Account {
     public int getBalance() {
         return balance;
     }
+
     public void deposit(int amount) {
-        balance += amount;
-        if (balance > 0 ){
-            debit = false;
-        }
+        int balanceBeforeDeposit = balance;
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Stan konta: " + balanceBeforeDeposit + " | " + "Wpłata: " + amount + " | Po operacji: " + balance);
+            if (balance > 0) {
+                debit = false;
+            }
+
+        } else System.out.println("Kwota wpłaty musi być dodatnia!");
     }
 
     public void withdraw(int amount) {
-        balance -= amount;
-        if(balance< 0){
-            debit = true;
-            System.out.println("Masz ujemny stan konta");
-        }
+        int balanceBeforeWithdraw = balance;
+
+        if (amount > 0) {
+            if (balance - amount < MAX_Debit) {
+                System.out.println("Nie można wykonać operacji przekraczającej debet");
+            } else {
+
+                balance -= amount;
+                System.out.println("Stan konta: " + balanceBeforeWithdraw + " | " + "Wypłata: " + amount + " | Po operacji: " + balance);
+
+                if (balance < 0) {
+                    debit = true;
+                    System.out.println("Masz ujemny stan konta");
+                }
+            }
+        } else System.out.println("Kwota wypłaty musi być dodatnia!");
+
+    }
+    public void transfer(Account other, int amount) {
+
+        if (balance - amount >= MAX_Debit) {
+            balance -=amount;
+            other.balance += amount;
+        }else System.out.println(" Nie można wykonać operacji - brak środków na koncie");
+    }
+
+    public String toString() {
+        return " Account{name: " + name + ", balance: " + balance + "}";
     }
 }
 
--- src/friends/firstpackage/AccountApplication.java
+++ src/friends/firstpackage/AccountApplication.java
@@ -6,5 +6,19 @@ public class AccountApplication {
         account.setName("Konto Premium");
         System.out.println("Nazwa: " + account.getName());
         System.out.println("Stan konta: " + account.getBalance());
+        Account otherAccount = new Account();
+        account.deposit(1250);
+        System.out.println(account);
+        account.deposit(-123);
+        account.withdraw(56);
+       // System.out.println(account);
+        account.withdraw(2000);
+        account.withdraw(300);
+        System.out.println(account);
+        System.out.println(otherAccount);
+        otherAccount.setName("konto inne");
+        account.transfer(otherAccount,150);
+        System.out.println(account);
+        System.out.println(otherAccount);
     }
 }
